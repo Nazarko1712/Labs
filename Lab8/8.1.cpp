063aae46c15b3aa35dad6102e4b5edd0a8371a0e
Alex <alexfrstnberg@gmail.com>
2019-12-02 17:43:04 +0200

    add lab8

---
+++ Lab8/8.1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+  setlocale(LC_ALL, "Russian");
+  srand(time(NULL));
+  int mn, m, n;
+  
+  
+  
+  float s = 0, sn = 0, k = 0, l = 0;
+  cout<<"Enter amount of elements: ";
+  cin>>mn;
+  while((float)sqrt(mn)!=(int)sqrt(mn)){
+  cout<<"\nImpossible to create a matrix :(( \nEnter amount of elements: ";
+  cin>>mn;
+  }
+  m=sqrt(mn);
+  n=m;
+  float arr[m][n];
+  cout << "? ??????? C (" <<m<<"x"<<n<<"):"<< endl;
+  
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      arr[i][j]=rand()%81-41;
+      cout << arr[i][j] << "\t";
+
+      if (i==j && arr[i][j] > 0) {
+        s = s + arr[i][j];
+      }
+     
+    }
+    cout << endl;
+  }
+
+  cout<<endl<<"????? ????????? ??????? ?????????: " <<s;
+  return 0;
+}
+
